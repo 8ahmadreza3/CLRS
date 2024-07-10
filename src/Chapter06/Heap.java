a9ba2376881a27718b1af2fe9df2a12ee3a9ea6a
8ahmadreza3 <8arhp3@gmail.com>
2024-07-10 15:58:37 +0330

    heap chapter 6

--- src/Chapter06/Heap.java
+++ src/Chapter06/Heap.java
@@ -1,9 +1,10 @@
 package Chapter06;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Heap {
-    static ArrayList<Integer> heap = new ArrayList<>();
+    public ArrayList<Integer> heap = new ArrayList<>();
 
     public int parent(int i){
         return i/2 ;
@@ -33,8 +34,8 @@ public class Heap {
 
     public void exchange(int i, int j){
         int temp = heap.get(i);
-        heap.add(i, heap.get(j));
-        heap.add(j, temp);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
     }
 
     public void buildMaxHeap(int[] a){
@@ -46,8 +47,55 @@ public class Heap {
         }
     }
 
-    public static void main(String[] args){
+    public void heapSort(int[] a){
+        buildMaxHeap(a);
+        for(int i=a.length-1 ; i>1 ; --i){
+            exchange(1, i);
+            maxHeapify(1);
+        }
+    }
 
+    public int heapMaximum(){
+        return heap.getFirst();
+    }
+
+    public int heapExtractMax(){
+        if(heap.isEmpty()){
+            return -1 ;
+        }
+        int max = heapMaximum();
+        int last = heap.size()-1 ;
+        heap.set(0, heap.get(last));
+        heap.remove(last);
+        maxHeapify(0);
+        return max ;
+    }
+
+    public void heapIncreaseKey(int i, int key){
+        if(key< heap.get(i)){
+            System.out.println("new key is smaller than current key");
+            return;
+        }
+        heap.set(i , key);
+        while(i>0 && heap.get(parent(i))<heap.get(i)){
+            exchange(i, parent(i));
+            i=parent(i);
+        }
+    }
+
+    public void maxHeapInsert(int key){
+        heap.add(Integer.MIN_VALUE);
+        heapIncreaseKey(heap.size()-1, key);
+    }
+
+    public static void main(String[] args){
+        int[] a = {5, 13, 2, 25, 7, 17, 20, 8, 4};
+        Heap h = new Heap();
+        h.heapSort(a);
+        h.heapExtractMax();
+        System.out.println(Arrays.toString(h.heap.toArray()));
+        h.heapIncreaseKey(3, 19);
+        System.out.println(Arrays.toString(h.heap.toArray()));
     }
 }
 
