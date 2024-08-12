dfae51fa6014ccba49cc6efe0603878b00b13d14
8ahmadreza3 <8arhp3@gmail.com>
2024-08-12 12:34:10 +0330

    lcs print

--- src/S04_AdvancedDesignAndAnalysisTechniques/Chapter15/LCS.java
+++ src/S04_AdvancedDesignAndAnalysisTechniques/Chapter15/LCS.java
@@ -24,11 +24,6 @@ public class LCS {
 
   public static int lcsBU(char[] x, char[] y, int m, int n){
     int[][] c = new int[m+1][n+1] ;
-    for(int i=0 ; i<=m ; i++)
-      c[i][0] = 0 ;
-
-    for(int j=0 ; j<=n ; j++)
-      c[0][j] = 0 ;
 
     for(int i = 0; i <= m; i++) {
       for (int j = 0; j <= n; j++) {
@@ -40,7 +35,32 @@ public class LCS {
           c[i][j] = Math.max(c[i - 1][j], c[i][j - 1]) ;
       }
     }
+    printLCS(x, y, m, n, c);
     return c[m][n] ;
+  }
+
+  static void printLCS(char[] x, char[] y, int m, int n, int[][] r){
+    int index = r[m][n];
+    int temp = index;
+    char[] lcs = new char[index + 1];
+
+    int i = m+1;
+    int j = n+1;
+    while (i > 0 && j > 0 && index > 0) {
+      if (x[i - 1] == y[j - 1]) {
+        lcs[index - 1] = x[i - 1];
+        i--;
+        j--;
+        index--;
+      } else if (r[i - 1][j] > r[i][j - 1])
+        i--;
+      else
+        j--;
+    }
+    System.out.print("LCS is : ");
+    for (int k = 0; k < temp; k++)
+      System.out.print(lcs[k]);
+    System.out.println();
   }
 
   public static void main(String[] args){
@@ -49,7 +69,7 @@ public class LCS {
     char[] y = " mohammadreza".toCharArray();
     int m = x.length ;
     int n = y.length ;
-    int[][] r = new int[m][n] ;
+    int[][] r = new int[m+1][n+1] ;
     System.out.println("Top Down: Longest common subsequence is: " + lcsTD(x, y, m-1, n-1, r)) ;
     System.out.println("Bottom Up: Longest common subsequence is: " + lcsBU(x, y, m-1, n-1)) ;
   }
