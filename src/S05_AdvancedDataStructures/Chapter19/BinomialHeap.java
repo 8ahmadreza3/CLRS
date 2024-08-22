79f8199a9c3de79aa2c100ff105e9429082613f4
8ahmadreza3 <8arhp3@gmail.com>
2024-08-22 17:59:25 +0330

    bional some methods

--- src/S05_AdvancedDataStructures/Chapter19/BinomialHeap.java
+++ src/S05_AdvancedDataStructures/Chapter19/BinomialHeap.java
@@ -79,4 +79,108 @@ public class BinomialHeap {
         }
     }
 
+    private void unionNodes(BinomialHeapNode binHeap) {
+        merge(binHeap);
+        BinomialHeapNode prevTemp = null, temp = Nodes,
+                nextTemp = Nodes.sibling;
+        while (nextTemp != null) {
+            if ((temp.degree != nextTemp.degree)
+                    || ((nextTemp.sibling != null)
+                    && (nextTemp.sibling.degree
+                    == temp.degree))) {
+                prevTemp = temp;
+                temp = nextTemp;
+            } else {
+                if (temp.key <= nextTemp.key) {
+                    temp.sibling = nextTemp.sibling;
+                    nextTemp.parent = temp;
+                    nextTemp.sibling = temp.child;
+                    temp.child = nextTemp;
+                    temp.degree++;
+                } else {
+                    if (prevTemp == null)
+                        Nodes = nextTemp;
+                    else
+                        prevTemp.sibling = nextTemp;
+
+                    temp.parent = nextTemp;
+                    temp.sibling = nextTemp.child;
+                    nextTemp.child = temp;
+                    nextTemp.degree++;
+                    temp = nextTemp;
+                }
+            }
+            nextTemp = temp.sibling;
+        }
+    }
+
+    public int findMinimum() {
+        return Nodes.findMinNode().key;
+    }
+
+    public void delete(int value) {
+        if ((Nodes != null)
+                && (Nodes.findANodeWithKey(value) != null)) {
+            decreaseKeyValue(value, findMinimum() - 1);
+            extractMin();
+        }
+    }
+
+    public void decreaseKeyValue(int old_value, int new_value) {
+        BinomialHeapNode temp = Nodes.findANodeWithKey(old_value);
+        if (temp == null)
+            return;
+
+        temp.key = new_value;
+        BinomialHeapNode tempParent = temp.parent;
+        while (tempParent != null && temp.key < tempParent.key) {
+            int z = temp.key;
+            temp.key = tempParent.key;
+            tempParent.key = z;
+            temp = tempParent;
+            tempParent = tempParent.parent;
+        }
+    }
+
+    public int extractMin() {
+        if (Nodes == null)
+            return -1;
+
+        BinomialHeapNode temp = Nodes, prevTemp = null;
+        BinomialHeapNode minNode = Nodes.findMinNode();
+        while (temp.key != minNode.key) {
+            prevTemp = temp;
+            temp = temp.sibling;
+        }
+        if (prevTemp == null)
+            Nodes = temp.sibling;
+        else
+            prevTemp.sibling = temp.sibling;
+
+        temp = temp.child;
+        BinomialHeapNode fakeNode = temp;
+
+        while (temp != null) {
+            temp.parent = null;
+            temp = temp.sibling;
+        }
+
+        if ((Nodes == null) && (fakeNode == null))
+            size = 0;
+        else {
+            if ((Nodes == null) && (fakeNode != null)) {
+                Nodes = fakeNode.reverse(null);
+                size = Nodes.getSize();
+            } else {
+                if ((Nodes != null) && (fakeNode == null))
+                    size = Nodes.getSize();
+                else {
+                    unionNodes(fakeNode.reverse(null));
+                    size = Nodes.getSize();
+                }
+            }
+        }
+        return minNode.key;
+    }
+
 }
